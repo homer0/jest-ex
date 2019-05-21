280b3fd1bcdf71ca09f3bdddf7ef5ef650306c4f
homer0 <leo.apiwan@olapic.com>
2019-05-21 03:05:58 -0300

    fix(project): remove shebang and fix typo on the prepare script for Windows

--- prepare.cmd
+++ prepare.cmd
@@ -1,9 +1,7 @@
-#!/bin/sh
-
-yarn --version >null 2>&1 && (
+yarn --version >NUL 2>&1 && (
   yarn run build
 ) || (
-  grep --version >null 2>&1 && (
+  grep --version >NUL 2>&1 && (
     npm run build
   ) || (
     echo You need either Yarn or WSL to work on windows
