013cdce8c34e440e1b735db378e7dfa670bf3a24
homer0 <leo.apiwan@olapic.com>
2020-03-25 00:38:49 -0300

    fix: use @jest/core for the runner

--- src/jest.js
+++ src/jest.js
@@ -1,7 +1,9 @@
-import jest from 'jest-cli';
 /**
  * The reason for this file is so I can test the runner class. In order to validate its
  * main function, I would need a mock of Jest, but if I mock it, the tests wouldn't work.
  * @ignore
  */
-export default jest;
+
+import { runCLI } from '@jest/core';
+
+export default { runCLI };
